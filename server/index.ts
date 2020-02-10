ca5d0fb590cb8a96777949e615041920758286f1
alexcravalho <a.d.cravalho@gmail.com>
2020-02-10 15:45:46 -0800

    changed server/index.js to typescript file;

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,14 @@
-// const compression = require('compression');
-const express = require('express');
+// import compression from 'compression';
+import express from 'express';
 const app = express();
 const PORT = 3000 || process.env.PORT;
-const path = require('path');
+import path from 'path';
 
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'dist')));
 app.use(express.json());
 // app.use(compression());
 
-app.get('/', (req, res) => {
-  console.log("got request");
-})
-
 app.listen(PORT, () => {
+  // tslint:disable-next-line:no-console
   console.log(`Express server is running on port: ${PORT}`);
 })
