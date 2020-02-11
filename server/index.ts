955f90194d7c166bd142d3b842484b6fde980bef
alexcravalho <a.d.cravalho@gmail.com>
2020-02-10 18:40:53 -0800

    added compression and cors middleware;

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,15 @@
-// import compression from 'compression';
+import compression from 'compression';
 import express from 'express';
-const app = express();
-const PORT = 3000 || process.env.PORT;
+import cors from 'cors';
 import path from 'path';
+const app = express();
+const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'dist')));
+app.use(cors({ credentials: true, origin: true }));
+
 app.use(express.json());
-// app.use(compression());
+app.use(compression());
 
 app.listen(PORT, () => {
   // tslint:disable-next-line:no-console
