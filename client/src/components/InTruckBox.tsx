0bff606e92b0819a3d7b5ed6f9f27031ca8dfa1a
alexcravalho <a.d.cravalho@gmail.com>
2020-03-02 18:02:00 -0800

    created InTruckBox component

---
+++ client/src/components/InTruckBox.tsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { TextField, FormLabel, FormGroup } from '@material-ui/core';
+import { LabledCheckbox } from './LabledCheckbox';
+
+interface InTruckBoxState {
+  labels: string[]
+}
+
+export class InTruckBox extends Component<{}, InTruckBoxState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      labels: [
+        "Garden hose w/ nozzle",
+        "Tarps x 2",
+        "Blower packs with FULL tanks",
+        "Gas can 3/4 full",
+        "Fire extinguisher",
+        "NO Garbage"
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="in-truck-box">
+        <h3>In Truck Box</h3>
+        {this.state.labels.map((label, idx) => (
+          <LabledCheckbox key={idx} label={label}/>
+        ))}
+      </div>
+    )
+  }
+};
