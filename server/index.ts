0843962797ed0816ae092e2f48e8fe49fdd62662
alexcravalho <a.d.cravalho@gmail.com>
2020-03-05 20:13:34 -0800

    added express post route

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,9 @@ import compression from 'compression';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import PDFDocument from 'pdfkit';
+import { nodemailerSendEmail } from './nodemailer'
+
 const app = express();
 const { PORT = 3000 } = process.env;
 
@@ -9,7 +12,25 @@ app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'dist')));
 app.use(cors({ credentials: true, origin: true }));
 
 app.use(express.json());
-app.use(compression());
+// app.use(compression());
+
+app.post('/pdf', (req, res) => {
+// create a blank pdf with pdfkit
+  const doc = new PDFDocument();
+  // doc.pipe(fs.createWriteStream('output.pdf'))
+// add an image to the blank pdf
+  doc.image(req.body.image, {
+    align: 'center',
+    valign: 'center'
+  });
+// finalize the pdf
+  doc.end();
+// add the new PDF to the request object
+  req.body.document = doc;
+  // send the completed pdf doc to the specified email
+  nodemailerSendEmail(req.body).catch((err: any) => { console.log(err) })
+  //
+})
 
 app.listen(PORT, () => {
   // tslint:disable-next-line:no-console
