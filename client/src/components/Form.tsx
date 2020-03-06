75d73f5b49668bfbf1f8d3f6f45a6875ab1230f2
alexcravalho <a.d.cravalho@gmail.com>
2020-03-05 20:16:29 -0800

    added handleChange function and more state

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import html2canvas from 'html2canvas';
+import axios from 'axios';
 import { BeforeStartup } from './BeforeStartup';
 import { InsideFromDrivers } from './InsideFromDrivers';
 import { UnderSeat } from './UnderSeat';
@@ -9,16 +11,40 @@ import { InTruckBox } from './InTruckBox';
 import { PassengerSide } from './PassengerSide';
 import { SubmitButton } from './SubmitButton';
 
-export class Form extends Component<{}, {}> {
+interface AppState {
+  driver: string,
+  truck: string,
+  date: string
+}
+
+export class Form extends Component<{}, AppState> {
   constructor(props: {}) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      driver: '',
+      truck: '',
+      date: ''
+    }
   }
+
+  handleChange = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ ...this.state, [event.target.name]: event.target.value });
+  };
 
   handleSubmit = () => {
-
-  }
+    const printForm = document.getElementById('app');
+    html2canvas(printForm)
+      .then(canvas => {
+        const formImage = canvas.toDataURL('image/png');
+        axios.post('/pdf', {
+          image: formImage,
+          // name:
+        })
+        .then((response) => { console.log(response) })
+        .catch((err) => { console.log(err) })
+      });
+  };
 
   render() {
     return (
@@ -31,14 +57,14 @@ export class Form extends Component<{}, {}> {
               <OutsideDriver />
             </div>
             <div className="form-right-column">
-              <TruckDriver />
+              <TruckDriver handleChange={this.handleChange}/>
               <Back />
               <InTruckBox />
               <PassengerSide />
             </div>
         </div>
         <div className="submission">
-          <SubmitButton handleSubmit={this.handleSubmit}/>
+          {/* <SubmitButton handleSubmit={this.handleSubmit}/> */}
         </div>
       </div>
     )
