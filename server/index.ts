5a2f2da96cb421e156d68622cb288304f372f604
alexcravalho <a.d.cravalho@gmail.com>
2020-03-12 15:03:36 -0700

    edited app.post

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import compression from 'compression';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import PDFDocument from 'pdfkit';
 import { nodemailerSendEmail } from './nodemailer'
 
 const app = express();
@@ -11,26 +10,18 @@ const { PORT = 3000 } = process.env;
 app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'dist')));
 app.use(cors({ credentials: true, origin: true }));
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb'}));
 // app.use(compression());
 
 app.post('/pdf', (req, res) => {
-// create a blank pdf with pdfkit
-  const doc = new PDFDocument();
-  // doc.pipe(fs.createWriteStream('output.pdf'))
-// add an image to the blank pdf
-  doc.image(req.body.image, {
-    align: 'center',
-    valign: 'center'
-  });
-// finalize the pdf
-  doc.end();
-// add the new PDF to the request object
-  req.body.document = doc;
+// send the pdf over nodemailer
+
+
   // send the completed pdf doc to the specified email
   // tslint:disable-next-line:no-console
-  nodemailerSendEmail(req.body).catch((err: any) => { console.log(err) })
-  //
+  // nodemailerSendEmail(req.body).catch((err: any) => { console.log(err) })
+  // // log success if there are no errors
+  // res.status(200).send("nodemailerSent an email to the in mail system")
 })
 
 app.listen(PORT, () => {
