ad44bb27360e1617ec3fa6bf23ef291cb230af3e
alexcravalho <a.d.cravalho@gmail.com>
2020-03-20 18:22:17 -0700

    added appStateHandleCheck to props; added idx to LabledCheckboxes

--- client/src/components/BeforeStartup.tsx
+++ client/src/components/BeforeStartup.tsx
@@ -7,6 +7,7 @@ import { PowerSteering } from './PowerSteering';
 
 interface StartupProps {
   appStateChange: any,
+  appStateHandleCheck: any,
   notes: string
 }
 
@@ -32,11 +33,11 @@ export class BeforeStartup extends Component<StartupProps, StartupState> {
       <div className="before-startup">
         <h3>Before Start up Driver or Certified Helper</h3>
         {this.state.labels.map((label, idx) => (
-          <LabledCheckbox key={idx} label={label} text=''/>
+          <LabledCheckbox key={idx} idx={idx} label={label} text='' appStateHandleCheck={this.props.appStateHandleCheck}/>
         ))}
-        <OilAdded />
-        <Coolant />
-        <PowerSteering />
+        <OilAdded appStateHandleCheck={this.props.appStateHandleCheck}/>
+        <Coolant appStateHandleCheck={this.props.appStateHandleCheck}/>
+        <PowerSteering appStateHandleCheck={this.props.appStateHandleCheck}/>
         <FormGroup className="notes-group" row>
           <FormLabel className="label" style={{ color: 'black' }}>Notes: </FormLabel>
           <TextField className="notes" multiline rows="5" variant="outlined" value={this.props.notes} onChange={(event: React.ChangeEvent<HTMLInputElement>)=> {this.props.appStateChange(event, 'notes')}} />
