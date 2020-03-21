5cbf1bbcd8aa80fdeb27ff98bd4def3304af28e4
alexcravalho <a.d.cravalho@gmail.com>
2020-03-20 18:21:26 -0700

    added CoolantProps; added appStateHandleCheck; added idx to LabeledCheckboxes

--- client/src/components/Coolant.tsx
+++ client/src/components/Coolant.tsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { FormLabel, FormGroup } from '@material-ui/core';
 import { LabledCheckbox } from './LabledCheckbox';
 
+interface CoolantProps {
+  appStateHandleCheck: any
+}
+
 interface CoolantState {
   options: string[]
 }
 
-export class Coolant extends Component<{}, CoolantState> {
-  constructor(props: {}) {
+export class Coolant extends Component<CoolantProps, CoolantState> {
+  constructor(props: CoolantProps) {
     super(props);
 
     this.state = {
@@ -21,7 +25,7 @@ export class Coolant extends Component<{}, CoolantState> {
         <FormGroup row>
           <FormLabel className="label" style={{color: 'black', lineHeight: '2.5'}}>Coolant: </FormLabel>
           {this.state.options.map((option, idx) => (
-            <LabledCheckbox key={idx} label={option} text="Coolant"/>
+            <LabledCheckbox key={idx} idx={idx} label={option} text="Coolant" appStateHandleCheck={this.props.appStateHandleCheck}/>
           ))}
         </FormGroup>
       </div>
