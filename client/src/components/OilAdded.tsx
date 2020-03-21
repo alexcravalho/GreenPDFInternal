56177863cdb8d70b337669d0fe862e3f9e92f9ec
alexcravalho <a.d.cravalho@gmail.com>
2020-03-20 18:16:02 -0700

    added OilAddedProps; added appStateHandleCheck; added idx to LabledCheckbox

--- client/src/components/OilAdded.tsx
+++ client/src/components/OilAdded.tsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { FormLabel, FormGroup } from '@material-ui/core';
 import { LabledCheckbox } from './LabledCheckbox';
 
+interface OilAddedProps {
+  appStateHandleCheck: any
+}
+
 interface OilAddedState {
   options: string[]
 }
 
-export class OilAdded extends Component<{}, OilAddedState> {
-  constructor(props: {}) {
+export class OilAdded extends Component<OilAddedProps, OilAddedState> {
+  constructor(props: OilAddedProps) {
     super(props);
 
     this.state = {
@@ -21,7 +25,7 @@ export class OilAdded extends Component<{}, OilAddedState> {
         <FormGroup className="oil-added" row>
           <FormLabel className="label" style={{color: 'black', lineHeight: '2.5'}}>Oil Added: </FormLabel>
           {this.state.options.map((option, idx) => (
-            <LabledCheckbox key={idx} label={option} text="Oil Added"/>
+            <LabledCheckbox key={idx} idx={idx} label={option} text="Oil Added" appStateHandleCheck={this.props.appStateHandleCheck}/>
           ))}
         </FormGroup>
       </div>
