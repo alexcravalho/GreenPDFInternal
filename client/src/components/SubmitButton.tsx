09c020b9671ce060f3781ac899064e44608866fa
alexcravalho <a.d.cravalho@gmail.com>
2020-03-22 19:29:29 -0700

    edited button color with material ui

--- client/src/components/SubmitButton.tsx
+++ client/src/components/SubmitButton.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
 
 interface SubmitButtonProps {
   handleSubmit: any
 }
 
+const StyledButton = styled(Button)({
+  background: '#ee1d23',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+});
+
 export const SubmitButton = (props: SubmitButtonProps) => (
       <div className="submit">
-        <Button fullWidth={true} variant="contained" color="primary" size="large" onClick={(event: React.MouseEvent<HTMLButtonElement>) => { props.handleSubmit() }}>Submit</Button>
+        <StyledButton fullWidth={true} variant="contained" size="large" onClick={(event: React.MouseEvent<HTMLButtonElement>) => { props.handleSubmit() }}>Submit</StyledButton>
       </div>
 );
