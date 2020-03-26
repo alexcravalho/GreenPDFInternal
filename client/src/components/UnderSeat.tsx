4a2db5110e3786cb2803cfd0432f325b096cafd4
alexcravalho <a.d.cravalho@gmail.com>
2020-03-25 17:01:22 -0700

    refactored Underseat as functional component

--- client/src/components/UnderSeat.tsx
+++ client/src/components/UnderSeat.tsx
@@ -1,39 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { LabledCheckbox } from './LabledCheckbox';
 
 interface UnderSeatProps {
   appStateHandleCheck: any
 }
 
-interface UnderSeatState {
-  labels: string[]
-}
+const labels = [
+  "Test light / Ear muffs x 2 / Duct tape x 2",
+  "Hard hats with safety vest inside x 3",
+  "Antifreeze jug Green/Orange",
+  "Oil pads minimum 10",
+  "First aid kit / fire extinguisher",
+  "Stop / Slow sign behind seat",
+  "Honk Horn exit"
+]
 
-export class UnderSeat extends Component<UnderSeatProps, UnderSeatState> {
-  constructor(props: UnderSeatProps) {
-    super(props);
-
-    this.state = {
-      labels: [
-        "Test light / Ear muffs x 2 / Duct tape x 2",
-        "Hard hats with safety vest inside x 3",
-        "Antifreeze jug Green/Orange",
-        "Oil pads minimum 10",
-        "First aid kit / fire extinguisher",
-        "Stop / Slow sign behind seat",
-        "Honk Horn exit"
-      ]
-    }
-  }
-
-  render() {
-    return (
+export const UnderSeat = (props: UnderSeatProps) => (
       <div className="under-seat">
         <h3>Under seat</h3>
-        {this.state.labels.map((label, idx) => (
-          <LabledCheckbox key={idx} idx={idx} label={label} text="" appStateHandleCheck={this.props.appStateHandleCheck}/>
+        {labels.map((label, idx) => (
+          <LabledCheckbox key={idx} idx={idx} label={label} text="" appStateHandleCheck={props.appStateHandleCheck}/>
         ))}
       </div>
-    )
-  }
-};
+    );
