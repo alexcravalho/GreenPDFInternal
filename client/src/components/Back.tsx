fbeaa87f47f98f2711e95de25236de71321d55ea
alexcravalho <a.d.cravalho@gmail.com>
2020-05-01 14:06:57 -0700

    edited text in Back

--- client/src/components/Back.tsx
+++ client/src/components/Back.tsx
@@ -21,15 +21,15 @@ export class Back extends Component<BackProps, BackState> {
 
     this.state = {
       labels1: [
-        "Brake-Tail-Center-Plate lights",
-        "Beacons flashers"
+        "Brake Tail Center Plate Lights",
+        "Beacons & Flashers"
       ],
       labels2: [
-        "hoses and couplers",
-        "hose adaptor",
-        "clean out bar",
-        "bumper clean",
-        "Climb ladder:"
+        "Hoses & Couplers",
+        "Hose Adaptor",
+        "Clean Out Bar",
+        "Bumper Clean",
+        "Climb Ladder:"
       ],
       options: ['Yes', 'No']
     }
@@ -38,23 +38,23 @@ export class Back extends Component<BackProps, BackState> {
   render() {
     return (
       <div className="back">
-        <h3>Back</h3>
+        <h3>Exterior Rear Check by Driver</h3>
         {this.state.labels1.map((label, idx) => (
           <LabledCheckbox key={idx} idx={idx} label={label} text="" appStateHandleCheck={this.props.appStateHandleCheck}/>
         ))}
         <FormGroup row>
-          <FormLabel className="label" style={{color: 'black', lineHeight: '4', fontWeight:500}}>License plate tabs expire: </FormLabel>
+          <FormLabel className="label" style={{color: 'black', lineHeight: '4', fontWeight:500}}>License Plate Tabs Expire: </FormLabel>
           <TextField className="tabs" label="MM/DD/YYYY" value={this.props.tabs} onChange={(event: React.ChangeEvent<HTMLInputElement>)=> {this.props.appStateChange(event, 'tabs')}} />
         </FormGroup>
         {this.state.labels2.map((label, idx) => (
           <LabledCheckbox key={idx} idx={idx} label={label} text="" appStateHandleCheck={this.props.appStateHandleCheck}/>
         ))}
         <FormGroup row>
-          <FormLabel className="label" style={{color: 'black', lineHeight: '2', fontWeight:500}}>Empty or preloaded with: </FormLabel>
+          <FormLabel className="label" style={{color: 'black', lineHeight: '2', fontWeight:500}}>Empty or Preloaded with: </FormLabel>
           <TextField className="pre" value={this.props.pre} onChange={(event: React.ChangeEvent<HTMLInputElement>)=> {this.props.appStateChange(event, 'pre')}} />
         </FormGroup>
         <FormGroup row>
-          <FormLabel className="label" style={{color: 'black', lineHeight: '2.5', fontWeight:500}}>Box bars straight: </FormLabel>
+          <FormLabel className="label" style={{color: 'black', lineHeight: '2.5', fontWeight:500}}>Box Bars Straight: </FormLabel>
           {this.state.options.map((option, idx) => (
             <LabledCheckbox key={idx} idx={idx} label={option} text="Box bars straight" appStateHandleCheck={this.props.appStateHandleCheck}/>
           ))}
